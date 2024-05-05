6ba47c3370b7114a06ec95686eac269863c5d197
gomez.145978 <gomez.145978@e.unavarra.es>
2024-05-05 19:58:02 +0200

    [23] - Añadidos tests unitarios para ApiClient

---
+++ tests/Unit/Streams/ApiClientTests.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Streams;
+
+use App\Services\ApiClient;
+use Spatie\FlareClient\Api;
+use Tests\TestCase;
+
+class StreamsComponentsTests extends TestCase
+{
+    /**
+     * @test
+     */
+    public function givenApiClientInvalidTokenReturnsInvalidOAuthTokenMessage(){
+        $apiClient = new ApiClient();
+        $url = "https://api.twitch.tv/helix/streams";
+        $badToken = "iHateSnickers";
+
+        $header = array(
+            'Authorization: Bearer ' . $badToken,
+        );
+
+        $response = $apiClient->makeCurlCall($url, $header);
+        $response = json_decode($response, true);
+        $this->assertEquals("Invalid OAuth token", $response["message"]);
+    }
+
+    /**
+     * @test
+     */
+    public function givenApiClientInvalidUrlReturnsFalse()
+    {
+        $apiClient = new ApiClient();
+        $badUrl = "hdfjskl";
+        $token = $apiClient->getToken("https://id.twitch.tv/oauth2/token");
+
+        $header = array(
+            'Authorization: Bearer ' . $token,
+        );
+
+        $response = $apiClient->makeCurlCall($badUrl, $header);
+        $this->assertFalse($response);
+    }
+
+    /**
+     * @test
+     */
+    public function givenBadUrlInGetTokenReturnsFalse(){
+        $apiClient = new ApiClient();
+        $badUrl = "hdfjskl";
+        $response = $apiClient->getToken($badUrl);
+        $this->assertFalse($response);
+    }
+
+}
