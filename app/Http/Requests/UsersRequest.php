5bf4b655b6f260211dad5926e5b0c2619d5f9cb1
Ivan Sanz <ivansanzsanzo@gmail.com>
2024-05-12 10:16:53 +0200

    [32] - Test unitarios funcionales

--- app/Http/Requests/UsersRequest.php
+++ app/Http/Requests/UsersRequest.php
@@ -25,7 +25,7 @@ class UsersRequest extends FormRequest
     {
         return [
             //
-            'id' => 'required|numeric',
+            'id' => 'required|integer',
         ];
     }
 
