8ee398960fe25b571fb39a9a3bf68b355f412784
gomez.145978 <gomez.145978@e.unavarra.es>
2024-06-02 15:02:14 +0200

    [61] - Tests para FollowRequest añadidos

--- app/Http/Requests/FollowRequest.php
+++ app/Http/Requests/FollowRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class FollowRequest extends FormRequest
 {
@@ -10,6 +12,11 @@ class FollowRequest extends FormRequest
     {
         return true;
     }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * @return array<string, ValidationRule|array|string>
+     */
 
     public function rules(): array
     {
@@ -19,6 +26,9 @@ class FollowRequest extends FormRequest
         ];
     }
 
+    /**
+     * @throws HttpResponseException
+     */
     protected function failedValidation($validator)
     {
         throw new HttpResponseException(response()->json([
