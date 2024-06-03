eb6e835a105a297c2301974bef75d1ea491cc224
luciazcona <azcona.142016@gmail.com>
2024-06-03 14:29:00 +0200

    [72] Controller modificado

--- app/Http/Infrastructure/Controllers/Controller.php
+++ app/Http/Infrastructure/Controllers/Controller.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Infrastructure\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Exception;
+
 abstract class Controller
 {
-    //
+    public function getUserStreamers(): JsonResponse
+    {
+        try {
+            $users = User::all();
+
+            return response()->json($users);
+        } catch (Exception) {
+            return response()->json(['error' => 'Error del servidor al obtener la lista de usuarios.'], 500);
+        }
+    }
 }
